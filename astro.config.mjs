836721c249908c8b3bcd35158dc5d59562af12eb
Yusuf Emre Albayrak <ysfemrealbyrk@gmail.com>
2026-05-06 20:18:21 +0300

    css fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://4PP-Studio.github.io',
-  base: '/4ppStudio-webpage',
+  site: 'https://4pp-studio.github.io',
+  base: '/4ppStudio-webpage/',
 });
